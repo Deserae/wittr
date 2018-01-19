66b7988b6a644c53fcbdc7a12813e69824b96a53
Deserae <deseraedias@gmail.com>
2018-01-18 23:14:09 -0700

    hijacking requests with service workers 3.11

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,13 @@
 self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		new Response(
+			'<h1 class="a-winner-is-me"> Hello World </h1>', {
+				headers: {'Content-Type': 'text/html' }			
+		})
+	);
+	
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  console.log("this is a test");
 });
