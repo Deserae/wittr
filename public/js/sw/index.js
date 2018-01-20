a597ea2ce60b43e6d604ea4ad3e859833aed7966
Deserae <deseraedias@gmail.com>
2018-01-19 20:48:54 -0700

    still working on 3.13. Links to read

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,22 @@
 self.addEventListener('fetch', function(event) {
+//they said, event.request gives you information about the request
+//i woudl console.log(event) and or console.log(event.request)
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  console.log("event.request" + event.request);
+  var url = event.request.something;
+  var isJpeg = url.endsWith('.jpg');
+
+  if (isJpeg){
+
+	  event.respondWith(
+	    fetch('/imgs/dr-evil.gif')
+
+  	);
+  }
+
+  // event.respondWith(
+	 //    new Response('test');
+	 //    );
+
 });
